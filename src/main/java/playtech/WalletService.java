59d41798e1a111f82579e725e5179aee4c5b3598
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-06-24 00:27:23 +0300

    logging

--- src/main/java/playtech/WalletService.java
+++ src/main/java/playtech/WalletService.java
@@ -1,5 +1,7 @@
 package playtech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -16,6 +18,7 @@ import java.math.BigDecimal;
 @Scope("request")
 @Path("/")
 public class WalletService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WalletService.class);
     private static final int NOT_FOUND = 1;
 
     private final PlayerAccess playerAccess;
@@ -32,6 +35,9 @@ public class WalletService {
                                       @PathParam("transactionId") final int transactionId,
                                       @PathParam("balanceChange") final BigDecimal balanceChange) {
 
+        System.out.println(LOGGER);
+        LOGGER.info("IN: transaction {} for user {}: requested balance change: {}",
+                new Object[]{transactionId, username, balanceChange});
         final WalletStatus status = new WalletStatus();
         status.transactionId = transactionId;
 
@@ -40,6 +46,8 @@ public class WalletService {
             player = playerAccess.load(username);
         } catch (EmptyResultDataAccessException exception) {
             status.errorCode = NOT_FOUND;
+            LOGGER.info("OUT: transaction {} for user {}: error code: {}",
+                    new Object[]{status.transactionId, username, status.errorCode});
             return status;
         }
 
@@ -50,6 +58,9 @@ public class WalletService {
         status.balanceVersion = player.balanceVersion;
         status.balanceChange = balanceChange;
         status.balanceAfterChange = player.balance;
+        LOGGER.info("OUT: transaction {} for user {}: balance {} of version {} after change {}",
+                new Object[]{status.transactionId, username,
+                        status.balanceAfterChange, status.balanceVersion, status.balanceChange});
         return status;
     }
 }
